6d6c9e37e107366bf31097424cf545fbb5dd2b18
Yassine Mouddene <yassinemouddene@gmail.com>
2022-11-30 18:40:24 +0100

    set-up initial project structure

---
+++ src/test/java/uir/info/projetintegre/ProjetS7ApplicationTests.java
@@ -0,0 +1,13 @@
+package uir.info.projetintegre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetS7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
